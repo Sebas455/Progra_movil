fdb9ed91381a2ded12abf45c2780a0387c34b680
Sebas455 <smolinal2300@alumno.ipn.mx>
2025-09-28 22:36:19 -0600

    primer commit

---
+++ DNI/dniscript.js
@@ -0,0 +1,25 @@
+const btnVerficar = document.getElementById("button-verficacion");
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+
+btnVerficar.addEventListener("click", () => {
+    alert(validacionDNI());
+});
+
+function validacionDNI() {
+    const numDNI = document.getElementById("text-numeroDNI").value;
+    const letraDNI = document.getElementById("text-letraDNI").value;
+
+    if (numDNI.trim() === "" || letraDNI.trim() === "" || !Number.isInteger(Number(numDNI))) {
+        return "Verifique la entrada";
+    }
+
+    if (Number(numDNI) < 0 || Number(numDNI) > 99999999) {
+        return "Formato de número fuera de rango";
+    }
+
+    if (letraDNI.trim().toUpperCase() === letras[Number(numDNI) % 23]) {
+        return "El DNI y la letra es correcta";
+    } else {
+        return "El DNI y la letra no es correcta";
+    }
+}
